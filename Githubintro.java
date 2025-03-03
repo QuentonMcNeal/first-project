699729df67e3f27fc9542526acb335bfd89e992a
Quenton McNeal <qmcneal@csus.edu>
2025-03-03 10:17:44 -0800

    Add files via upload

---
+++ Githubintro.java
@@ -0,0 +1,14 @@
+	import java.util.Scanner;
+public class Githubintro {
+		public static void main(String[] args) {
+			Scanner scan = new Scanner(System.in); 
+			System.out.println("Hello what is your first name?");
+			String name = scan.nextLine();
+			System.out.print( "Hello, ");
+			System.out.print(name );
+			System.out.print(" this is my first code on Github.");
+		}
+
+	}
+
+
